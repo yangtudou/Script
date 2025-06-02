032c36e2c8cd3a154296acdcf53c2615b9b185c6
yang_potato <yangtudou1220@icloud.com>
2025-06-02 23:16:24 +0800

    --

--- Stash/js/my-ip.js
+++ Stash/js/my-ip.js
@@ -1,6 +1,11 @@
 $httpClient.get('http://httpbin.org/get', (error, response, data) => {
   if (error) {
-    console.log(error)
+    $done({
+      title: '测试脚本',
+      content: data,
+      backgroundColor: '#663399',
+      icon: 'network',
+    })
   } else {
     $done({
       title: '测试脚本',
