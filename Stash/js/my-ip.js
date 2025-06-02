f784a9e71393f726fd05eff89cd71502ba343420
yang_potato <yangtudou1220@icloud.com>
2025-06-02 23:05:19 +0800

    -

--- Stash/js/my-ip.js
+++ Stash/js/my-ip.js
@@ -1,8 +1,32 @@
-$httpClient.get('https://api.my-ip.io/ip', function (error, response, data) {
-  $done({
-    title: '当前 IP 地址',
-    content: data,
-    backgroundColor: '#663399',
-    icon: 'network',
-  })
+$httpClient.get('http://httpbin.org/get', (error, response, data) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(data)
+  }
 })
+ 
+const yourProxyName = 'a fancy name with 😄'
+ 
+$httpClient.post(
+  {
+    url: 'http://httpbin.org/post',
+    headers: {
+      'X-Header-Key': 'headerValue',
+      'X-Stash-Selected-Proxy': encodeURIComponent(yourProxyName),
+    },
+    body: '{}', // can be object or string
+    timeout: 5,
+    insecure: false,
+    'binary-mode': true,
+    'auto-cookie': true,
+    'auto-redirect': true,
+  },
+  (error, response, data) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(data)
+    }
+  }
+)
