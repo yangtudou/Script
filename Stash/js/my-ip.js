c928b1ae3f9b438ec378a0e59d1abce7675cac61
yang_potato <yangtudou1220@icloud.com>
2025-06-02 23:12:30 +0800

    test

--- Stash/js/my-ip.js
+++ Stash/js/my-ip.js
@@ -2,31 +2,11 @@ $httpClient.get('http://httpbin.org/get', (error, response, data) => {
   if (error) {
     console.log(error)
   } else {
-    console.log(data)
+    $done({
+      title: '测试脚本',
+      content: data,
+      backgroundColor: '#663399',
+      icon: 'network',
+    })
   }
 })
- 
-const yourProxyName = 'a fancy name with 😄'
- 
-$httpClient.post(
-  {
-    url: 'http://httpbin.org/post',
-    headers: {
-      'X-Header-Key': 'headerValue',
-      'X-Stash-Selected-Proxy': encodeURIComponent(yourProxyName),
-    },
-    body: '{}', // can be object or string
-    timeout: 5,
-    insecure: false,
-    'binary-mode': true,
-    'auto-cookie': true,
-    'auto-redirect': true,
-  },
-  (error, response, data) => {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(data)
-    }
-  }
-)
